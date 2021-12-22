2307bc0a4d8036a112753e04a6ff3cbce148c3a0
Veronika Starkulla <veronika.starkulla@gmx.de>
2021-12-22 10:08:35 +0100

    Solved Day02 Part02

--- src/Day02/Day02.java
+++ src/Day02/Day02.java
@@ -13,8 +13,35 @@ public class Day02 {
         BufferedReader bufferedReader = new BufferedReader(new FileReader("txtInputData/Day02/input.txt"));
         List<String> inputStringList = readInputTxt(bufferedReader);
 
+//        Part 1
         int position = countPosition(inputStringList);
+        
+//        Part 2
+        int position2 = countPosition2(inputStringList);
+
         System.out.println(position);
+        System.out.println(position2);
+    }
+
+    public static int countPosition2(List<String> input) {
+        int horizontal = 0;
+        int depth = 0;
+        int aim = 0;
+
+        for (String entry : input) {
+
+            String[] entryArr = entry.split(" ");
+            if (entryArr[0].equals("forward")) {
+                horizontal += Integer.parseInt(entryArr[1]);
+                depth += aim * Integer.parseInt(entryArr[1]);
+            }else if (entryArr[0].equals("down")) {
+                aim += Integer.parseInt(entryArr[1]);
+            }else if (entryArr[0].equals("up")) {
+                aim -= Integer.parseInt(entryArr[1]);
+            }
+        }
+
+        return horizontal * depth;
     }
 
     public static int countPosition(List<String> input) {
