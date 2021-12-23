b176645461c25a47f9dbd1deb071339c9f9c25e0
Veronika Starkulla <veronika.starkulla@gmx.de>
2021-12-23 20:47:27 +0100

    Refactor Day03 Part01

--- src/Day03/Day03.java
+++ src/Day03/Day03.java
@@ -4,6 +4,7 @@ import java.io.BufferedReader;
 import java.io.FileReader;
 import java.io.IOException;
 import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.List;
 
 public class Day03 {
@@ -13,12 +14,25 @@ public class Day03 {
         BufferedReader bufferedReader = new BufferedReader(new FileReader("txtInputData/Day03/input.txt"));
         List<String> inputStringList = readInputTxt(bufferedReader);
 
-//        Part 1
+        List<int[]> zerosAndOnes = countZerosAndOnes(inputStringList);
+        int[] sumZero = zerosAndOnes.get(0);
+        int[] sumOne = zerosAndOnes.get(1);
 
-//        Count all zeros and ones
-        int[] sumZero = new int[inputStringList.get(0).length()];
-        int[] sumOne = new int[inputStringList.get(0).length()];
-        for (String entry : inputStringList) {
+        List<int[]> gammaAndEpsilon = detectGammaAndEpsilon(sumZero, sumOne);
+        int[] gammaRate = gammaAndEpsilon.get(0);
+        int[] epsilonRate = gammaAndEpsilon.get(1);
+        
+        int gamma = convertToDecimal(gammaRate);
+        int epsilon = convertToDecimal(epsilonRate);
+
+        System.out.println(gamma*epsilon);
+    }
+
+    public static List<int[]> countZerosAndOnes(List<String> input) {
+        List<int[]> zerosAndOnes = new ArrayList<>();
+        int[] sumZero = new int[input.get(0).length()];
+        int[] sumOne = new int[input.get(0).length()];
+        for (String entry : input) {
             for (int i = 0; i < entry.length(); i++) {
                 if (entry.charAt(i) == '0') {
                     sumZero[i] += 1;
@@ -27,13 +41,27 @@ public class Day03 {
                 }
             }
         }
+        zerosAndOnes.add(sumZero);
+        zerosAndOnes.add(sumOne);
+        return zerosAndOnes;
+    }
 
-//        save most common bit in gammaRate and least common bit in epsilonRate
-        int[] gammaRate = new int[inputStringList.get(0).length()];
-        int[] epsilonRate = new int[inputStringList.get(0).length()];
+    public static int convertToDecimal(int[] binary) {
+        int decimal = 0;
+        for(int i = 0; i < binary.length; i++) {
+            if (binary[i] == 1) {
+                decimal += Math.pow(2,binary.length-1-i);
+            }
+        }
+        return decimal;
+    }
 
-        for (int i = 0; i < sumZero.length; i++) {
-            if (sumZero[i] > sumOne[i]) {
+    public static List<int[]> detectGammaAndEpsilon(int[] zero, int[] one) {
+        int[] gammaRate = new int[zero.length];
+        int[] epsilonRate = new int[zero.length];
+
+        for (int i = 0; i < zero.length; i++) {
+            if (zero[i] > one[i]) {
                 gammaRate[i] = 0;
                 epsilonRate[i] = 1;
             } else {
@@ -41,23 +69,7 @@ public class Day03 {
                 epsilonRate[i] = 0;
             }
         }
-
-//        convert from binary to decimal
-        int gamma = 0;
-        for(int i = 0; i < gammaRate.length; i++) {
-            if (gammaRate[i] == 1) {
-                gamma += Math.pow(2,gammaRate.length-1-i);
-            }
-        }
-
-        int epsilon = 0;
-        for(int i = 0; i < epsilonRate.length; i++) {
-            if (epsilonRate[i] == 1) {
-                epsilon += Math.pow(2,epsilonRate.length-1-i);
-            }
-        }
-
-        System.out.println(gamma * epsilon);
+        return Arrays.asList(gammaRate, epsilonRate);
     }
 
     public static List<String> readInputTxt(BufferedReader br) throws IOException {
